fa13eebf9c8299b8b575827456f2d298491a80a3
stephen-harlow <srh150030@utdallas.edu>
2016-03-26 21:51:54 -0500

    First Zip

--- samples/src/main/java/helloworld/HelloWorldSpeechlet.java
+++ samples/src/main/java/helloworld/HelloWorldSpeechlet.java
@@ -9,6 +9,8 @@
  */
 package helloworld;
 
+import java.util.Map;
+
 import com.amazon.speech.slu.Intent;
 import com.amazon.speech.slu.Slot;
 import com.amazon.speech.speechlet.*;
@@ -23,11 +25,31 @@ import java.util.Date;
 /**
  * This sample shows how to create a simple speechlet for handling speechlet requests.
  *
+ List
+    Add
+    Remove
+    Open
+        Add List Object to Session
+        "What would you like to do with this list?"
+
+ Task
+    Get Task
+        Search by Tasks
+    Complete Task
+    Create Task
+        If List
+            Add to List
+        Else
+            Add to Inbox
+    Update Task
 
  */
 //
 public class HelloWorldSpeechlet implements Speechlet {
     private static final Logger log = LoggerFactory.getLogger(HelloWorldSpeechlet.class);
+
+    private static final String LIST_KEY = "LIST";
+    private static final String LIST_SLOT = "List";
 
     @Override
     public void onSessionStarted(final SessionStartedRequest request, final Session session)
@@ -55,12 +77,27 @@ public class HelloWorldSpeechlet implements Speechlet {
         Intent intent = request.getIntent();
         String intentName = (intent != null) ? intent.getName() : null;
         String[] intentNames = {"event", "date", "timeStart", "duration"};
-        if ("CreateEventIntent".equals(intentName)) {
+
+        if ("OpenListIntent".equals(intentName)) {
+            Slot slot = intent.getSlot("event");
+            if (slot != null && slot.getValue() != null) {
+                return setListInSession(intent, session);
+            }
+            else{
+                String speechText =
+                        "I'm not quite sure what list you were looking for";
+                String repromptText =
+                        "Please try again. ";
+
+                return getSpeechletResponse(speechText, repromptText, true);
+
+            }
+        }else if ("CreateEventIntent".equals(intentName)) {
             String empt = "";
 
             for(int i = 0; i < 4; i++)
             {
-                Slot slot = intent.getSlot(intentName[i]);
+                Slot slot = intent.getSlot(intentNames[i]);
                 if(slot != null && slot.getValue() != null) {
                     intentValues[i] = slot.getValue().toString();
                 }
@@ -81,15 +118,70 @@ public class HelloWorldSpeechlet implements Speechlet {
                 }
             }
 
-            return getHelloResponse(empt);
+            return getViewResponse(empt);
         }
         else if ("AMAZON.HelpIntent".equals(intentName)) {
             return getHelpResponse();
         } else {
             throw new SpeechletException("Invalid Intent");
         }
-    }
 
+
+    }
+    private SpeechletResponse setListInSession(final Intent intent, final Session session) {
+        // Get the slots from the intent.
+        Map<String, Slot> slots = intent.getSlots();
+
+        // Get the color slot from the list of slots.
+        Slot favoriteListSlot = slots.get(LIST_SLOT);
+        String speechText, repromptText;
+
+        // Check for favorite color and create output to user.
+        if (favoriteListSlot != null) {
+            // Store the user's favorite color in the Session and create response.
+            String listname = favoriteListSlot.getValue();
+            session.setAttribute(LIST_KEY, listname);
+            speechText =
+                    String.format("I have now opened the list called " + listname);
+            repromptText =
+                    "What would you like to do with the list?";
+
+        } else {
+            // Render an error since we don't know what the users favorite color is.
+            speechText = "I'm not sure what list you are trying to open. ";
+            repromptText =
+                    "What list would you like to open? ";
+        }
+
+        return getSpeechletResponse(speechText, repromptText, true);
+    }
+    /**
+     * Returns a Speechlet response for a speech and reprompt text.
+     */
+    private SpeechletResponse getSpeechletResponse(String speechText, String repromptText,
+                                                   boolean isAskResponse) {
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("Session");
+        card.setContent(speechText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        if (isAskResponse) {
+            // Create reprompt
+            PlainTextOutputSpeech repromptSpeech = new PlainTextOutputSpeech();
+            repromptSpeech.setText(repromptText);
+            Reprompt reprompt = new Reprompt();
+            reprompt.setOutputSpeech(repromptSpeech);
+
+            return SpeechletResponse.newAskResponse(speech, reprompt, card);
+
+        } else {
+            return SpeechletResponse.newTellResponse(speech, card);
+        }
+    }
     @Override
     public void onSessionEnded(final SessionEndedRequest request, final Session session)
             throws SpeechletException {
@@ -128,7 +220,7 @@ public class HelloWorldSpeechlet implements Speechlet {
      * @return SpeechletResponse spoken and visual response for the given intent
      */
     private SpeechletResponse getCreationResponse(String[] item) {
-        String speechText = item;
+        String speechText = item[0];
 
         // Create the Simple card content.
         SimpleCard card = new SimpleCard();
